da7d666be8b4514c641058cc82bb578cc9cc0b4b
Dong <1025737796@qq.com>
2022-04-12 14:30:24 +0800

    新增讲师列表查询、删除讲师接口

--- service/service-edu/src/main/java/cn/yd/eduservice/controller/EduTeacherController.java
+++ service/service-edu/src/main/java/cn/yd/eduservice/controller/EduTeacherController.java
@@ -1,9 +1,8 @@
 package cn.yd.eduservice.controller;
-
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import cn.yd.commonVo.Result;
+import cn.yd.eduservice.service.EduTeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -14,8 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-04-12
  */
 @RestController
-@RequestMapping("/eduservice/edu-teacher")
+@RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
+    @Autowired
+    private EduTeacherService eduTeacherService;
 
+    @GetMapping("/list")
+    public Result<?> teacherList() {
+        return Result.OK(eduTeacherService.list(null));
+    }
+
+    @DeleteMapping("{id}")
+    public Result<?> deleteTeacher(@PathVariable String id){
+         eduTeacherService.removeById(id);
+         return Result.OK("删除成功");
+    }
 }
 
