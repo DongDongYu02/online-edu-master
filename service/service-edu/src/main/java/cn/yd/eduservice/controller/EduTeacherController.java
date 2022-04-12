e82ef75271f819251b78d9aad0f82d4f326bbb8e
Dong <1025737796@qq.com>
2022-04-12 11:03:36 +0800

    生成讲师mapper service controller

---
+++ service/service-edu/src/main/java/cn/yd/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,21 @@
+package cn.yd.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author Dong
+ * @since 2022-04-12
+ */
+@RestController
+@RequestMapping("/eduservice/edu-teacher")
+public class EduTeacherController {
+
+}
+
---
+++ service/service-edu/src/main/java/cn/yd/eduservice/service/impl/EduTeacherServiceImpl.java
@@ -0,0 +1,20 @@
+package cn.yd.eduservice.service.impl;
+
+import cn.yd.eduservice.entity.EduTeacher;
+import cn.yd.eduservice.mapper.EduTeacherMapper;
+import cn.yd.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 讲师 服务实现类
+ * </p>
+ *
+ * @author Dong
+ * @since 2022-04-12
+ */
+@Service
+public class EduTeacherServiceImpl extends ServiceImpl<EduTeacherMapper, EduTeacher> implements EduTeacherService {
+
+}
