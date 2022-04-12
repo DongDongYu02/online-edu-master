eec99d7055e9c8d26b7c2d1afeaf18765d283a45
Dong <1025737796@qq.com>
2022-04-12 14:31:30 +0800

    添加同意返回结果实体

---
+++ common/common-utils/src/main/java/cn/yd/commonutils/ResultCode.java
@@ -0,0 +1,13 @@
+package cn.yd.commonutils;
+
+public interface ResultCode {
+    /**
+     * 成功状态码
+     */
+    public static Integer OK = 200;
+
+    /**
+     * 失败状态码
+     */
+    public static Integer ERROR = 500;
+}
